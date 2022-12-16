45499259decd808e28b9c23825fd05b1a2112604
Sergey7819 <socrat366@icloud.com>
2022-12-16 11:57:38 +0300

    project commit

---
+++ src/main/java/org/hibernate/Main.java
@@ -0,0 +1,26 @@
+package org.hibernate;
+
+import org.hibernate.dao.UserDao;
+import org.hibernate.dao.UserDaoHibernateImpl;
+import org.hibernate.entity.User;
+
+
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserDao userDao = new UserDaoHibernateImpl();
+
+        userDao.dropUsersTable();
+        userDao.createUsersTable();
+        userDao.saveUser("jack", "Richer", (byte) 45);
+        userDao.saveUser("Ramesh", "Fadatare", (byte) 25);
+
+        List<User> userList = userDao.getAllUsers();
+        for (User u : userList) {
+            System.out.println(u.toString());
+        }
+    }
+}
---
+++ src/main/java/org/hibernate/dao/UserDao.java
@@ -0,0 +1,21 @@
+package org.hibernate.dao;
+
+
+import org.hibernate.entity.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    void createUsersTable();
+
+    void dropUsersTable();
+
+    void saveUser(String name, String lastName, byte age);
+
+    void removeUserById(long id);
+
+    List<User> getAllUsers();
+
+    void cleanUsersTable();
+}
